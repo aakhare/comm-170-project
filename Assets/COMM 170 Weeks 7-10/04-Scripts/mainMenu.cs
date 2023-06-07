68ec4e4f5ef7600f538f9e8f641d3123105238e9
aakhare <aastha.khare@gmail.com>
2023-06-07 02:49:59 -0700

    Catered everything more to UP theme

--- Assets/COMM 170 Weeks 7-10/04-Scripts/mainMenu.cs
+++ Assets/COMM 170 Weeks 7-10/04-Scripts/mainMenu.cs
@@ -13,6 +13,23 @@ public class mainMenu : MonoBehaviour
     }
 
     public void PlayButton(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Instructions");
+    }
+
+    public void Continue1(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Instr2");
+    }
+
+    public void Continue2(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Instr3");
+        Debug.Log("Continue2: loading Instr3");
+    }
+
+    public void Continue3(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Instr4");
+    }
+
+    public void Continue4(){
         UnityEngine.SceneManagement.SceneManager.LoadScene("TrashTest");
     }
     
